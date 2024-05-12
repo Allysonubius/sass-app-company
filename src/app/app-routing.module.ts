c9c24deb4979863dd1f8bd1dc7cb820b02d02082
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2024-05-12 15:26:48 -0300

    adicionado path de redirect para tela home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,11 @@ import { ContactComponent } from './Pages/contact/contact.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
     path:'home',
     component: HomeComponent
   },
