56c85c12f45bd517e9b1d767898f049c8ed147d4
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2024-05-12 16:15:11 -0300

    adicionado tela not-found meme

--- src/app/Components/not-found/not-found.component.ts
+++ src/app/Components/not-found/not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -6,12 +6,17 @@ import { Title } from '@angular/platform-browser';
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent implements OnInit {
+export class NotFoundComponent implements AfterViewInit {
 
+  @ViewChild('videoPlayer') videoPlayerRef!: ElementRef;
   constructor(private titleService: Title) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.titleService.setTitle('Not Found Page')
+    if (this.videoPlayerRef && this.videoPlayerRef.nativeElement) {
+      const videoElement: HTMLVideoElement = this.videoPlayerRef.nativeElement;
+      videoElement.volume = 0.5; // Definindo o volume para 50%
+    }
   }
 
 }
