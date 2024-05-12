9642bd3dfca0a51a273f7a5a669c3c6534e782de
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2024-05-12 16:24:05 -0300

    Trocado video para gif

--- src/app/Components/not-found/not-found.component.ts
+++ src/app/Components/not-found/not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -6,17 +6,12 @@ import { Title } from '@angular/platform-browser';
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent implements AfterViewInit {
+export class NotFoundComponent implements OnInit {
 
-  @ViewChild('videoPlayer') videoPlayerRef!: ElementRef;
   constructor(private titleService: Title) { }
 
-  ngAfterViewInit(): void {
+  ngOnInit(): void {
     this.titleService.setTitle('Not Found Page')
-    if (this.videoPlayerRef && this.videoPlayerRef.nativeElement) {
-      const videoElement: HTMLVideoElement = this.videoPlayerRef.nativeElement;
-      videoElement.volume = 0.5; // Definindo o volume para 50%
-    }
   }
 
 }
